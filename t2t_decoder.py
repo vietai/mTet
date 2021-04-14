4878ddffd24d7e5bb3684bb0b8f7be199e853199
ntkchinh <ngokieuchinh89@gmail.com>
2021-04-14 16:24:31 +0700

    Update t2t_decoder.py
    add transformer_tall9()

--- t2t_decoder.py
+++ t2t_decoder.py
@@ -14,6 +14,15 @@ from tensor2tensor.utils import registry
 flags = tf.flags
 FLAGS = flags.FLAGS
 
+@registry.register_hparams
+def transformer_tall9():
+  hparams = transformer.transformer_big()
+  hparams.hidden_size = 768
+  hparams.filter_size = 3072
+  hparams.num_hidden_layers = 9
+  hparams.num_heads = 12
+  return hparams
+
 if __name__ == '__main__':
   tf.logging.set_verbosity(tf.logging.INFO)
   # tf.app.run(t2t_decoder.main)
