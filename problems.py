efb4c1bf70037cb7ad4d6ce1259131ffd7c5f3b6
ntkchinh <ngokieuchinh89@gmail.com>
2021-05-19 11:36:11 +0700

    Update problems.py
    update problems translate_class11_pure_envi/vien

--- problems.py
+++ problems.py
@@ -6,8 +6,8 @@ from __future__ import division
 from tensor2tensor.data_generators import translate_envi
 from tensor2tensor.utils import registry
 
-
 # ----- Problem for augmented Data
+
 @registry.register_problem
 class TranslateClass11AppendtagVienIwslt32k(translate_envi.TranslateEnviIwslt32k):
   """Problem spec for IWSLT'15 En-Vi translation."""
@@ -33,7 +33,9 @@ class TranslateClass11AppendtagEnviIwslt32k(translate_envi.TranslateEnviIwslt32k
     # This error will not be raised unless the t2t_datagen with this problem is called.
     raise NotImplementedError('Please specify append and style-tagging data augmented files for training.')
 
+
 # ----- Problem for original Data
+
 @registry.register_problem
 class TranslateClass11PureVienIwslt32k(translate_envi.TranslateEnviIwslt32k):
   """Problem spec for IWSLT'15 En-Vi translation."""
@@ -43,8 +45,8 @@ class TranslateClass11PureVienIwslt32k(translate_envi.TranslateEnviIwslt32k):
     return 2**15  # 32768
 
   def source_data_files(self, dataset_split):
-    # This error will not be raised unless the t2t_datagen with this problem is called.
-    raise NotImplementedError('Please specify data files for training.')
+    train = dataset_split == translate_envi.problem.DatasetSplit.TRAIN
+    return ['', ('train.vi', 'train.en')]] if train else [['',('tst2012.vi', 'tst2012.en')]]
 
 
 @registry.register_problem
@@ -56,5 +58,5 @@ class TranslateClass11PureEnviIwslt32k(translate_envi.TranslateEnviIwslt32k):
     return 2**15  # 32768
 
   def source_data_files(self, dataset_split):
-    # This error will not be raised unless the t2t_datagen with this problem is called.
-    raise NotImplementedError('Please specify data files for training.')
+    train = dataset_split == translate_envi.problem.DatasetSplit.TRAIN
+    return ['', ('train.en', 'train.vi')]] if train else [['',('tst2012.en', 'tst2012.vi')]]
